5301db9dab89d12a96e6e19d91459c882f673117
AxelGutierrezV <u21222595@utp.edu.pe>
2026-07-17 03:19:35 -0500

    fix: cors conections

--- cart-service/index.js
+++ cart-service/index.js
@@ -8,7 +8,15 @@ const routes = require("./routes/cartRoutes");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: 
+[
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "https://panchitostore-shop.netlify.app",
+      "https://panchitostore-admin.netlify.app"
+    ]
+,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
--- inventory-service/index.js
+++ inventory-service/index.js
@@ -8,10 +8,15 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5175",
+    origin: 
+[
+      "http://localhost:5173",
       "http://localhost:5174",
-      "http://localhost:5173"
-    ],
+      "http://localhost:5175",
+      "https://panchitostore-shop.netlify.app",
+      "https://panchitostore-admin.netlify.app"
+    ]
+,
     credentials: true,
   })
 );
