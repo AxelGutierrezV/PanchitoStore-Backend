70b40477bc7d1553a1a59be28a4744c87f7053fe
AxelGutierrezV <u21222595@utp.edu.pe>
2026-07-20 18:01:15 -0500

    API Catalogs added

--- order-service/routes/orderRoutes.js
+++ order-service/routes/orderRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const controller = require("../controllers/orderController");
 
-router.post("/", controller.createOrder);
-router.get("/", controller.getOrders);
-router.post("/from-cart", controller.createOrderFromCart);
-router.get("/stats",controller.getOrderStats);
-router.patch("/:id/status",controller.updateOrderStatus);
-router.get("/:id/detail",controller.getOrderDetail);
-router.get("/:id", controller.getOrderById);
-router.get("/client/:clienteId",controller.getOrdersByClient);
+    router.post("/", controller.createOrder);
+    router.get("/", controller.getOrders);
+    router.post("/from-cart", controller.createOrderFromCart);
+    router.get("/stats",controller.getOrderStats);
+    router.patch("/:id/status",controller.updateOrderStatus);
+    router.get("/:id/detail",controller.getOrderDetail);
+    router.get("/:id", controller.getOrderById);
+    router.get("/client/:clienteId",controller.getOrdersByClient);
 module.exports = router;
